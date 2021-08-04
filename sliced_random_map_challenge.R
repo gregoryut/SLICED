ce60237f20825ebbe102c83cda9dd2d81e3f1162
Gregory Utkin <grishautkin@gmail.com>
2021-08-04 12:03:16 -0400

    a quick challenge on one of the  Sliced nights

---
+++ sliced_random_map_challenge.R
@@ -0,0 +1,78 @@
+library(tidyverse)
+library(here)
+library(sf)
+library(tigris)
+library(leaflet)
+
+
+
+df <- read_csv("https://raw.githubusercontent.com/magi-1/presidential-elections/main/Data/TAMIDS%20Given/county_level.csv")
+
+
+ct <- tigris::counties(cb = TRUE)
+
+
+df_sp <- ct %>%
+  right_join(df, by = c("GEOID"= "county_fips")) %>% 
+  filter(STATEFP != 15) %>%
+  mutate(p_diff = votes_gop/total_votes) 
+
+o <- df_sp %>%
+  filter(STATEFP != 15) %>%
+  mutate(p_diff = votes_gop/total_votes) %>%
+  ggplot(aes(fill = p_diff)) +
+  geom_sf(lwd = 0.4) +
+  borders("state") +  
+  scale_fill_distiller(palette = "RdBu") + 
+  coord_sf(crs = 5070) +
+  theme(plot.background = element_rect(fill = "black", color = "black")) +
+  theme_minimal()
+
+o
+
+ggsave("my_plot.png", device = "png", dpi = 450, bg = "white")
+ 
+
+leaflet(df_sp) %>%
+  addProviderTiles(providers$Stamen.Toner) %>%
+  addPolygons(fillColor = ~p_diff)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
